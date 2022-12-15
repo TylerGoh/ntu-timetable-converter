b4d2652a7e3d9816e414f298a0283d72a447175a
TylerGoh <tylergoh50@gmail.com>
2022-12-15 18:04:49 +0800

    Added Nodemon and Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 const Express = require("express");
+const mongoose = require("mongoose");
 const expressApp = new Express();
 const EXPRESS_APP_PORT = 5000;
+const URI = process.env.MONGO_DB_URI;
+mongoose.connect(URI, { useNewUrlParse: true }, () => {
+  console.log("Connected to MongoDB");
+});
 
 expressApp.listen(EXPRESS_APP_PORT, () =>
   console.log(`Express App is running on ${EXPRESS_APP_PORT}`)
